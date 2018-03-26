2608cd440151d3b21abb8eda416882816c6543c9
Ács Dávid <acsdavid97@gmail.com>
2018-03-26 22:29:18 +0300

    main app code

--- code/StudentManagementApplication.java
+++ code/StudentManagementApplication.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import business.Facade;
+import business.LoginBLL;
+import presentation.controller.LoginController;
+import presentation.view.LoginView;
+
+import javax.swing.*;
 
 public class StudentManagementApplication {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        LoginView loginView = new LoginView();
+        LoginController controller = new LoginController(loginView, facade);
+    }
 }
