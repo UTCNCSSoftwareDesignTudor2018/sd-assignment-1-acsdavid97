c59b38d303c98f56e93e4327efc2d3aa1ce97db3
Ács Dávid <acsdavid97@gmail.com>
2018-03-28 17:22:46 +0300

    documentation updated to reflect new implementation

---
+++ README.txt
@@ -0,0 +1,8 @@
+Install MySQL server, run the database.sql
+Build the project, run the application:
+As student:
+    username: user
+    password: uu
+As teacher:
+    username: teacher
+    teacher: teacher
--- doc/database.sql
+++ doc/database.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `tucn` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `tucn`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: tucn
@@ -30,8 +28,8 @@ CREATE TABLE `courses` (
   `teacher_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_courses_teachers1_idx` (`teacher_id`),
-  CONSTRAINT `fk_courses_teachers1` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_courses_teachers1` FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,37 +38,37 @@ CREATE TABLE `courses` (
 
 LOCK TABLES `courses` WRITE;
 /*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (2,'SD',1);
+INSERT INTO `courses` VALUES (2,'SD',1),(3,'Not SD',1),(4,'asdf',1),(5,'sdfa',1),(6,'sdaf',1),(7,'fdh',1),(8,'fdsgf',1),(9,'asdf',1),(10,'kjldaslfa',4);
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `enrollements`
+-- Table structure for table `enrollments`
 --
 
-DROP TABLE IF EXISTS `enrollements`;
+DROP TABLE IF EXISTS `enrollments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `enrollements` (
+CREATE TABLE `enrollments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `student_id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`student_id`,`course_id`),
   KEY `fk_students_has_courses_courses1_idx` (`course_id`),
   KEY `fk_students_has_courses_students_idx` (`student_id`),
-  CONSTRAINT `fk_students_has_courses_courses1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_students_has_courses_students` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_students_has_courses_courses1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_students_has_courses_students` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `enrollements`
+-- Dumping data for table `enrollments`
 --
 
-LOCK TABLES `enrollements` WRITE;
-/*!40000 ALTER TABLE `enrollements` DISABLE KEYS */;
-INSERT INTO `enrollements` VALUES (2,1,2);
-/*!40000 ALTER TABLE `enrollements` ENABLE KEYS */;
+LOCK TABLES `enrollments` WRITE;
+/*!40000 ALTER TABLE `enrollments` DISABLE KEYS */;
+INSERT INTO `enrollments` VALUES (28,1,2),(29,1,3),(31,5,5),(32,5,6);
+/*!40000 ALTER TABLE `enrollments` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -83,11 +81,12 @@ DROP TABLE IF EXISTS `exams`;
 CREATE TABLE `exams` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `grade` int(11) NOT NULL,
-  `enrollement_id` int(11) NOT NULL,
+  `enrollment_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_exams_enrollements1_idx` (`enrollement_id`),
-  CONSTRAINT `fk_exams_enrollements1` FOREIGN KEY (`enrollement_id`) REFERENCES `enrollements` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `fk_exams_enrollements1_idx` (`enrollment_id`),
+  CONSTRAINT `fk_exams_enrollements1` FOREIGN KEY (`enrollment_id`) REFERENCES `enrollments` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,6 +95,7 @@ CREATE TABLE `exams` (
 
 LOCK TABLES `exams` WRITE;
 /*!40000 ALTER TABLE `exams` DISABLE KEYS */;
+INSERT INTO `exams` VALUES (1,0,28,'2018-03-28 08:18:38'),(2,9,28,'2018-03-28 08:25:13'),(3,9,29,'2018-03-28 08:25:49'),(4,10,31,'2018-03-28 09:10:50');
 /*!40000 ALTER TABLE `exams` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,7 +111,7 @@ CREATE TABLE `logins` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +120,7 @@ CREATE TABLE `logins` (
 
 LOCK TABLES `logins` WRITE;
 /*!40000 ALTER TABLE `logins` DISABLE KEYS */;
-INSERT INTO `logins` VALUES (1,'user','user'),(2,'teacher','teacher');
+INSERT INTO `logins` VALUES (1,'user','uu'),(2,'teacher','teacher'),(3,'stud2','ss'),(4,'asdfk','askldfj'),(5,'aklsjfd','kajsldf'),(8,'user23','user'),(9,'suer3','kalsdfj'),(12,'asdf','asdf');
 /*!40000 ALTER TABLE `logins` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,8 +137,8 @@ CREATE TABLE `students` (
   `id_number` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_students_users1_idx` (`user_id`),
-  CONSTRAINT `fk_students_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_students_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,7 +147,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,1,'32113');
+INSERT INTO `students` VALUES (1,1,'32113'),(5,8,'replace'),(6,9,'replace');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -163,8 +163,8 @@ CREATE TABLE `teachers` (
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_teachers_users1_idx` (`user_id`),
-  CONSTRAINT `fk_teachers_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_teachers_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -173,7 +173,7 @@ CREATE TABLE `teachers` (
 
 LOCK TABLES `teachers` WRITE;
 /*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
-INSERT INTO `teachers` VALUES (1,2);
+INSERT INTO `teachers` VALUES (1,2),(4,12);
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,8 +193,8 @@ CREATE TABLE `users` (
   `login_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_users_user_logins1_idx` (`login_id`),
-  CONSTRAINT `fk_users_user_logins1` FOREIGN KEY (`login_id`) REFERENCES `logins` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_users_user_logins1` FOREIGN KEY (`login_id`) REFERENCES `logins` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -203,7 +203,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'User','12345','12345','User address',1),(2,'Teacher','54321','54321','Teacher address',2);
+INSERT INTO `users` VALUES (1,'User2','123456','123456','User address6',1),(2,'Teacher','54321','54321','Teacher address',2),(3,'stud','1231','342','324',3),(4,'askdlfj','asldkfj','ljkdasf','aklsdjf',4),(5,'kjasdf','kljds','kjl','lkasdfj',5),(8,'user','user','user','user',8),(9,'klasdjf','lkdsafkj','klasdjf','k',9),(12,'asdf','asdf','asdf','asdf',12);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -216,4 +216,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-26 19:28:36
+-- Dump completed on 2018-03-28 17:15:33
